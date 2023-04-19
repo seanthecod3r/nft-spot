992bb209d5862a0d894701f96810aa737da0e743
seanthecod3r <sean.celli@yahoo.com>
2023-04-19 03:02:26 -0700

    Added functionality to receive articles from test db and display them all

--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Article } from 'src/app/shared/models/article';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.css']
+  styleUrls: ['./articles.component.css'],
 })
 export class ArticlesComponent {
+  articles: Article[];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    return this.http
+      .get<Article[]>('http://localhost:3000/Articles')
+      .subscribe((articles) => {
+        console.log(articles);
+        this.articles = articles;
+      });
+  }
 }
